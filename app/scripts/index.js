39ce856b07242998de145a3b0784b041ca58488e
Tyler Black <blacktylera@gmail.com>
2015-03-20 12:26:09 -0500

    images are now appending with phaser

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,14 +1,16 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv', { preload: preload, create: create, update: update });
-
+var game = new Phaser.Game(800, 674, Phaser.AUTO, 'gameDiv', { preload: preload, create: create, update: update });
+var bacon;
+var background;
 function preload() {
 
-    game.load.image('bacon', '../assets/bacon.png');
-    game.load.image('broccoli', '../assets/broccoli.png');
-    game.load.image('background', '../assets/backgroundstatic.png');
-    game.load.image('character', '../assets/pixelavatar96.png', 32, 48);
-
-
     game.stage.backgroundColor = '#71c5cf';
+    game.load.image('bacon', 'scripts/bacon.png');
+    game.load.image('broccoli', 'scripts/broccoli.png');
+    game.load.image('background', 'scripts/backgroundstatic.png');
+    game.load.image('character', 'scripts/pixelavatar96.png', 32, 48);
+
+
+
 }
 
 var platforms;
@@ -20,8 +22,8 @@ function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     //  Game Background
-    game.add.sprite(0, 0, 'background');
-    game.add.sprite(0, 0, 'bacon');
+    background = game.add.sprite(0, 0, 'background');
+    bacon = game.add.sprite(0, 0, 'bacon');
 
  }
 
